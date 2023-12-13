9e80db62d1765cdf6f116638aba970de94da455e
GideonRotic1 <rotichgid38@gmail.com>
2023-12-13 10:42:01 +0300

    Arithmetics lab submitted

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 =31;
+let num2 =2;
+let multiply =num1*num2;
 
-// Write your code here
+let random=0;
+random ++;
+
+let num3 =32;
+let num4 =7;
+let mod =num3 % num4;
+
+let max=Math.max(10,5,7,12,20,14,15)
+
+
